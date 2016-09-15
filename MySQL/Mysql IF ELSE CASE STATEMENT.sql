30a4291c3d4c57de5c68a5b6e853d34983c9839e
eugen <eugenr@clout.com>
2016-09-15 15:43:09 +0300

    09.15.16

--- MySQL/Mysql IF ELSE CASE STATEMENT.sql
+++ MySQL/Mysql IF ELSE CASE STATEMENT.sql
@@ -26,3 +26,24 @@ FROM clout_v1_3cron.store_schedule where clout_v1_3cron.store_schedule._user_id 
 
 
 
+--------------------------IF QUERY AND ORDER BY PRIORITY 
+
+
+SELECT id AS bank_id, institution_name AS bank_name, logo_url, institution_code AS bank_code, 
+
+	IF(institution_name = '1199 SEIU FCU (New York, NY)', 1,
+	IF(institution_name LIKE CONCAT('1199 SEIU FCU (New York, NY)','%'), 2, 
+	IF(institution_name LIKE CONCAT('%','1199 SEIU FCU (New York, NY)','%'), 3, 
+	IF(institution_name LIKE CONCAT('%','1199 SEIU FCU (New York, NY)'), 4, 
+	5)))) AS priority
+
+FROM banks 
+WHERE is_featured IN ('N') 
+AND institution_code <> ''
+ORDER BY priority, LENGTH(institution_name) 
+LIMIT 5
+
+
+
+
+
